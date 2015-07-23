c14656f9305a84c9fd87b6b7058e27863f1d0efb
Leptons <lepton.st@gmail.com>
2015-07-23 21:11:50 +0900

    implement methods

--- perl/lib/Log.pm
+++ perl/lib/Log.pm
@@ -8,18 +8,34 @@ sub new {
 }
 
 sub protocol {
+    my ($self) = @_;
+    my $protocol = (split ' ', $self->{req})[2];
+    return $protocol;
 }
 
 sub method {
+    my ($self) = @_;
+    my $method = (split ' ', $self->{req})[0];
+    return $method;
 }
 
 sub path {
+    my ($self) = @_;
+    my $path = (split ' ', $self->{req})[1];
+    return $path;
 }
 
 sub uri {
+    my ($self) = @_;
+    my $uri = 'http://' . $self->{host} . $self->path;
+    return $uri;
 }
 
 sub time {
+    my ($self) = @_;
+    my ($sec, $min, $hour, $day, $month, $year) = gmtime $self->{epoch};
+    my $time = sprintf "%04d-%02d-%02dT%02d:%02d:%02d", $year+1900, $month+1, $day, $hour, $min, $sec;
+    return $time;
 }
 
 1;

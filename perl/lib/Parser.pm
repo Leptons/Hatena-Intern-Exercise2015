97deee938767c0375cf9bf966a7c72feecf49897
Leptons <lepton.st@gmail.com>
2015-07-26 22:57:33 +0900

    implement 'parse' method

--- perl/lib/Parser.pm
+++ perl/lib/Parser.pm
@@ -2,12 +2,23 @@ package Parser;
 use strict;
 use warnings;
 
+use Log;
+
 sub new {
     my ($class, %args) = @_;
     return bless \%args, $class;
 }
 
 sub parse {
+    my ($self) = @_;
+    open my $fh, '<', $self->{filename} or die $!;
+    my @res;
+    while (<$fh>){
+        chomp;
+        my @log = map { /(.*?):(.*)/s; $1 => $2; } grep !/:-/, split /\t/;
+        push @res, Log->new(@log);
+    }
+    return \@res;
 }
 
 1;

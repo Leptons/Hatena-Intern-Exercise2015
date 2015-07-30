1c69f837ae66343c41116329555d167453bd15c7
Leptons <lepton.st@gmail.com>
2015-07-30 16:50:21 +0900

    implement methods

--- perl/lib/LogCounter.pm
+++ perl/lib/LogCounter.pm
@@ -8,9 +8,22 @@ sub new {
 };
 
 sub group_by_user {
+    my ($self) = @_;
+    my %res;
+    for my $log (@{$self->{logs}}){
+        my $user = $log->{user} // 'guest';
+        push @{$res{$user}}, $log;
+    }
+    return \%res;
 }
 
 sub count_error {
+    my ($self) = @_;
+    my $num_error = 0;
+    for my $log (@{$self->{logs}}){
+        $num_error++ if 500 <= $log->{status} && $log->{status} < 600;
+    }
+    return $num_error;
 }
 
 1;

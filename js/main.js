31a8d4d205e0edafe6233f49de396bfe9695f5be
Leptons <lepton.st@gmail.com>
2015-08-01 13:27:43 +0900

    implement parseLTSVLog

--- js/main.js
+++ js/main.js
@@ -1,3 +1,22 @@
-// 課題 JS-1: 関数 `parseLTSVLog` を記述してください
+function parseLTSVLog(logStr) {
+	var logs = logStr.split("\n");
+	logs.pop();
+	var logRecords = new Array;
+	for(var i = 0; i < logs.length; i++){
+		var keyValues = logs[i].split("\t");
+		var record = new Object;
+		for(var j = 0; j < keyValues.length; j++){
+			var result = keyValues[j].match(/^(.+?):(.*)$/);
+			if(result !== null && result[2] !== "-"){
+				if(result[1] === "epoch"){
+					result[2] = parseInt(result[2], 10);
+				} 
+				record[result[1]] = result[2];
+			}
+		}
+		logRecords.push(record);
+	}
+	return logRecords;
+}
 
 // 課題 JS-2: 関数 `createLogTable` を記述してください

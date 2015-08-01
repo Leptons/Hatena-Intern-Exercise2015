40bb4d691026afc004125d8f707954a0bcf1917d
Leptons <lepton.st@gmail.com>
2015-08-01 14:48:14 +0900

    implement createLogTable

--- js/main.js
+++ js/main.js
@@ -19,4 +19,39 @@ function parseLTSVLog(logStr) {
 	return logRecords;
 }
 
-// 課題 JS-2: 関数 `createLogTable` を記述してください
+function createLogTable(elem, logRecords) {
+	var pathText = document.createTextNode('path');
+	var pathNode = document.createElement('th');
+	pathNode.appendChild(pathText);
+
+	var epochText = document.createTextNode('epoch');
+	var epochNode = document.createElement('th');
+	epochNode.appendChild(epochText);
+
+	var pathEpochNode = document.createElement('tr');
+	pathEpochNode.appendChild(pathNode);
+	pathEpochNode.appendChild(epochNode);
+	var theadNode = document.createElement('thead');
+	theadNode.appendChild(pathEpochNode);
+
+	var tbodyNode = document.createElement('tbody');
+	for(var i = 0; i < logRecords.length; i++){
+		var text1 = document.createTextNode(logRecords[i].path);
+		var tdNode1 = document.createElement('td');
+		tdNode1.appendChild(text1);
+
+		var text2 = document.createTextNode(logRecords[i].epoch);
+		var tdNode2 = document.createElement('td');
+		tdNode2.appendChild(text2);
+
+		var trNode = document.createElement('tr');
+		trNode.appendChild(tdNode1);
+		trNode.appendChild(tdNode2);
+		tbodyNode.appendChild(trNode);
+	}
+
+	var tableNode = document.createElement('table');
+	tableNode.appendChild(theadNode);
+	tableNode.appendChild(tbodyNode);
+	elem.appendChild(tableNode);
+}

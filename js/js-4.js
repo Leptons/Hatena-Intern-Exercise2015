9bded157a49488059c542f4ca650ed44de7f7a5e
Leptons <lepton.st@gmail.com>
2015-08-02 18:58:10 +0900

    implement search

---
+++ js/js-4.js
@@ -0,0 +1,17 @@
+(function() {
+	function searchLogTable(){
+		var logInput = document.getElementById("log-input");
+		var tableContainer = document.getElementById("table-container");
+		var searchInput = document.getElementById("search-input");
+		var log = parseLTSVLog(logInput.value);
+		var searchText = searchInput.value;
+		var logFiltered = log.filter(function(elem){
+			return elem.path.indexOf(searchText)>=0;
+		});
+		createLogTable(tableContainer, logFiltered);
+	}
+
+	var searchButton = document.getElementById("search-button");
+	searchButton.addEventListener("click", searchLogTable, false);
+})();
+
